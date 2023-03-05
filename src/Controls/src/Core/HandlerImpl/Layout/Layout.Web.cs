9082b940f44473e5e8f9aeca56581cbe44183ffb
Frank A. Krueger <fak@praeclarum.org>
2023-03-05 11:13:16 -0800

    Add Web handlers based on Standard

---
+++ src/Controls/src/Core/HandlerImpl/Layout/Layout.Web.cs
@@ -0,0 +1,12 @@
+﻿#nullable disable
+namespace Microsoft.Maui.Controls
+{
+	public partial class Layout
+	{
+		public static void MapInputTransparent(LayoutHandler handler, Layout layout) { }
+
+		public static void MapInputTransparent(ILayoutHandler handler, Layout layout) { }
+
+		static void MapInputTransparent(IViewHandler handler, IView layout) { }
+	}
+}
---
+++ src/Core/src/Fonts/FontRegistrar.Web.cs
@@ -0,0 +1,10 @@
+#nullable enable
+
+namespace Microsoft.Maui
+{
+	/// <inheritdoc/>
+	public partial class FontRegistrar : IFontRegistrar
+	{
+		string? LoadNativeAppFont(string font, string filename, string? alias) => null;
+	}
+}
