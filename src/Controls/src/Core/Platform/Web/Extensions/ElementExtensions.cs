d639341ae1e115cc5d6fc809aa6b0e9f13970ae7
Frank A. Krueger <fak@praeclarum.org>
2023-03-12 14:36:52 -0700

    Add Element Extensions for future pain

---
+++ src/Controls/src/Core/Platform/Web/Extensions/ElementExtensions.cs
@@ -0,0 +1,11 @@
+#nullable disable
+using System;
+using Microsoft.Maui.Controls.Internals;
+using static Microsoft.Maui.Controls.Button;
+
+namespace Microsoft.Maui.Controls.Platform
+{
+    public static class ElementExtensions
+    {
+    }
+}
